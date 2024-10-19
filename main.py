ae4465018067dec0f0ce3d402344f7c65bc1e357
Bonitabueno <166970698+Bonitabueno@users.noreply.github.com>
2024-10-19 21:30:38 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+import pytz
+import FinanceDataReader as fdr
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+import pandas as pd
+import yfinance as yf
+import anthropic
